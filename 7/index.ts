1b4d5e5b1c61cd24dfc3c20eb200346d50e315f3
Krystof Liskovec <krystof.liskovec@gmail.com>
2022-12-07 21:21:39 +0100

    Add day 7

---
+++ 7/index.ts
@@ -0,0 +1,142 @@
+import console from "console";
+import fs from "fs/promises";
+import path from "path";
+
+async function start() {
+    const input = await fs.readFile(path.join(__dirname, "input.txt"), "utf8");
+
+    const lines = input.split("\n");
+
+    const fileTree: any = {};
+    let dirTraverse: string[] = [];
+
+    for(let i = 0; i < lines.length; i++) {
+        if(lines[i].includes("$ cd")) {
+            cd(dirTraverse, lines[i].split("$ cd ")[1]);
+        } else if (lines[i].includes("$ ls")) {
+            const output = ls(i, lines);
+            addOutputToFileTree(fileTree, dirTraverse, output);
+        }
+    }
+
+    const sizes = getDirectorySizes(fileTree, 100000);
+
+    if(Array.isArray(sizes))
+        console.log(sizes.reduce((curr, acc) => acc += curr, 0));
+
+    console.log(findDirectorySizeToDelete(fileTree));
+}
+
+function cd(dirTraverse: string[], targetDir: string) {
+    if(targetDir === "..") {
+        dirTraverse.pop();
+        return;
+    } else if (targetDir === "/") {
+        dirTraverse = [];
+        return;
+    }
+
+    dirTraverse.push(targetDir);
+}
+
+function ls(currentLine: number, lines: string[]) {
+    const linesToScan = lines.slice(currentLine + 1);
+
+    const output = [];
+    let lineProgress = 0;
+
+    while(true) {
+        if(linesToScan[lineProgress] === undefined) break;
+        if(linesToScan[lineProgress].includes("$")) {
+            break;
+        }
+
+        output.push(linesToScan[lineProgress]);
+
+        lineProgress++;
+    }
+
+    return output;
+}
+
+function addOutputToFileTree(fileTree: any, dirTraverse: string[], output: string[]): any {
+    if(fileTree === undefined) fileTree = {};
+
+    if(dirTraverse.length > 0) {
+        fileTree[dirTraverse[0]] = addOutputToFileTree(fileTree[dirTraverse[0]], dirTraverse.slice(1), output);
+        return fileTree;
+    }
+
+    for(const line of output) {
+        const [sizeOrType, name] = line.split(" ");
+        if(sizeOrType === "dir") {
+            fileTree[name] = {};
+            continue;
+        }
+
+        fileTree[name] = sizeOrType;
+    }
+
+    return fileTree;
+}
+
+function getDirectorySizes(fileTree: any, limit: number = Infinity, sumArray: number[] = [], root = true) {
+    let sum = 0;
+
+    for(const file in fileTree) {
+        if(Number.isNaN(Number(fileTree[file]))) {
+            const size = getDirectorySizes(fileTree[file], limit, sumArray, false);
+            if(!Array.isArray(size)) {
+                sum += size;
+            }
+            continue;
+        }
+
+        const value = Number(fileTree[file]);
+        sum += value;
+    }
+
+    if(sum <= limit) {
+        sumArray.push(sum);
+    }
+    
+    if(root) return sumArray;
+    return sum;
+}
+
+function getUsedSpace(fileTree: any, sum = 0) {
+    for(const file in fileTree) {
+        if(Number.isNaN(Number(fileTree[file]))) {
+            sum = getUsedSpace(fileTree[file], sum);
+            continue;
+        }
+
+        const value = Number(fileTree[file]);
+        sum += value;
+    }
+    
+    return sum;
+}
+
+function getFreeSpace(fileTree: any) {
+    return 70000000 - getUsedSpace(fileTree);
+}
+
+function findDirectorySizeToDelete(fileTree: any, requiredSpace = 30000000) {
+    const spaceNeeded = requiredSpace - getFreeSpace(fileTree);
+
+    const sizes = getDirectorySizes(fileTree);
+    let directorySize = Infinity;
+    if(Array.isArray(sizes)) {
+        for(const size of sizes) {
+            if(spaceNeeded <= size && directorySize > size) {
+                directorySize = size;
+            }
+        }
+    }
+
+    return directorySize;
+}
+
+
+start();
